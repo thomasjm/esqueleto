85ad0565cf057505bec78dcca104381e5c323bb1
Tom McLaughlin <pyro777@gmail.com>
2016-12-26 01:28:48 -0800

    Remove unused import

--- src/Database/Esqueleto/Internal/Language.hs
+++ src/Database/Esqueleto/Internal/Language.hs
@@ -52,7 +52,6 @@ import Database.Esqueleto.Internal.PersistentImport
 import Text.Blaze.Html (Html)
 
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 
